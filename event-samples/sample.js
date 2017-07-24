dfde3439642a5bc4b1a93a0db4b2d2f41c72d855
thekozak <kozak.kevin@gmail.com>
2017-07-24 13:37:03 -0500

    add lambda-local test and a sample

---
+++ event-samples/sample.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "session": {
+        "sessionId": "SessionId.5ab7b95a-4090-42e1-b5a4-eb3a0c5f5cdc",
+        "application": {
+            "applicationId": "amzn1.ask.skill.ca116d16-c7da-4513-bcbe-b78b35e75d25"
+        },
+        "attributes": {},
+        "user": {
+            "userId": "amzn1.ask.account.AFAOZSBPTQJPETW7EIPZNDSNT5LHJPZFEPICG34OTLEBRFP3765VG5MRHYWY3KQCDM5S3NOAWP6YFM2SQG4473FU2O6QGZ5XI5MKSRQ2NGGUVUQJOYP6SUFNHZX4WF6DBXOJXAKG2XDD7SNM3RQEOITVRUUFYIAJH33WLHWLK72AI4KEY3DOQKCJYF5DUFA4BTMYRF37FWMZTDQ"
+        },
+        "new": true
+    },
+    "request": {
+        "type": "IntentRequest",
+        "requestId": "EdwRequestId.4c119da6-006c-4887-9541-10787678551d",
+        "locale": "en-US",
+        "timestamp": "2017-07-22T18:30:15Z",
+        "intent": {
+            "name": "PlayAudio",
+            "slots": {}
+        }
+    },
+    "version": "1.0"
+};
