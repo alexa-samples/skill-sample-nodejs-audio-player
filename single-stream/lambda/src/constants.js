2f134ec1db61caa73e6752a3f387fb255392a8bf
Stormacq, Sebastien <stormacq@amazon.lu>
2017-10-08 11:06:36 +0100

    initial commit for single stream sample

---
+++ single-stream/lambda/src/constants.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = Object.freeze({
+    
+    //App-ID. TODO: set to your own Skill App ID from the developer portal.
+    //appId : 'amzn1.ask.skill.123',
+
+    // when true, the skill logs additional detail, including the full request received from Alexa
+    debug : false
+
+});
---
+++ single-stream/lambda/src/strings.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let EnglishStrings = {
+    "WELCOME_MSG": "Welcome to {{ skillName }}",
+    "HELP_MSG": "Welcome to {{ skillName }}. You can play, stop, resume listening.  How can I help you ?",
+    "UNHANDLED_MSG" : "Sorry, I could not understand what you've just said.",
+    "CAN_NOT_SKIP_MSG" : "This is radio, you have to wait for next track to play.",
+    "RESUME_MSG" : "Resuming {{ skillName }}",
+    "NOT_POSSIBLE_MSG" : "This is radio, you can not do that.  You can ask me to stop or pause to stop listening.",
+    "STOP_MSG" : "Goodbye."
+};
+module.exports = {
+    "en-GB": {
+        "translation": EnglishStrings
+    },
+    "en-US": {
+        "translation": EnglishStrings
+    }
+};
