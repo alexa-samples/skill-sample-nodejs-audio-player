036a26e25c7f450c51fed000d57a4c29bda73688
Stormacq, Sebastien <stormacq@amazon.lu>
2018-04-12 07:35:05 +0100

    fix bug for FR cards

--- single-stream/lambda/src/audioAssets.js
+++ single-stream/lambda/src/audioAssets.js
@@ -28,13 +28,28 @@ let de = { // TODO add german translation
     startJingle : 'https://s3.amazonaws.com/alexademo.ninja/maxi80/jingle.m4a'        
 } 
 
+let fr = { // TODO add german translation
+    card : {
+        title: 'Ma Radio',
+        subtitle: 'Moins de bla bla, plus de la la la',
+        cardContent: "Visitez notre site web https://www.myradio.com",
+        image: {
+            largeImageUrl: 'https://s3.amazonaws.com/alexademo.ninja/maxi80/alexa-artwork-1200.png',
+            smallImageUrl: 'https://s3.amazonaws.com/alexademo.ninja/maxi80/alexa-artwork-720.png'
+        }
+    },
+    url: 'https://audio1.maxi80.com',
+    startJingle : 'https://s3.amazonaws.com/alexademo.ninja/maxi80/jingle.m4a'        
+} 
+
 let globalAudioData = {
     'en-US': en,
     'en-GB': en,
     'en-CA': en,
     'en-IN': en,
     'en-AU': en,
-    'de-DE': de
+    'de-DE': de,
+    'fr-FR': fr
 };
 
 function audioData(request) {
