aebee82d4bc1a0ead3d4231cecc449e39de00285
Stormacq, Sebastien <stormacq@amazon.lu>
2018-04-23 15:39:49 +0100

    adjust table name to previous version

--- single-stream/lambda/src/Constants.ts
+++ single-stream/lambda/src/Constants.ts
@@ -13,7 +13,7 @@ export const Constants = {
     // when defined, it tries to read / write DynamoDB to save the last time Jingle was played for that user
     // this allows to avoid to repaet the jingle at each invocation 
     jingle : {
-        databaseTable : "myradio_jingle",
+        databaseTable : "my_radio",
         playOnceEvery : 1000 /*ms*/ * 60 * 60 * 24 // 24 hours
         //playOnceEvery : 1000 /*ms*/ * 60 * 3 // 3 minutes 
     }
